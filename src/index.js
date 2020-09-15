45c5a15d0ce132ab8976fb0fd600530f411c4922
pixulainen <george.apetrei@gmail.com>
2020-09-15 23:51:39 +0100

    graphql basics

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { GraphQLServer } from 'graphql-yoga';
+import db from './db';
+import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
+import User from './resolvers/User';
+import Post from './resolvers/Post';
+import Comment from './resolvers/Comment';
+const resolvers = {
+	Query,
+	Mutation,
+	User,
+	Post,
+	Comment
+};
+const server = new GraphQLServer({
+	typeDefs: './src/schema.graphql',
+	resolvers,
+	context: [ db ]
+});
+
+server.start(() => {
+	console.log('The server is up!');
+});
